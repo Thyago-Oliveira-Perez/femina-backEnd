346ba4008d8247149464db6f89afde6beb13a052
VitorDanielAdams <vitor.adams@outlook.com>
2022-04-04 20:02:38 -0300

    001

---
+++ src/main/java/br/com/femina/FeminaApplication.java
@@ -0,0 +1,13 @@
+package br.com.femina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeminaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeminaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/femina/FeminaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.femina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeminaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
