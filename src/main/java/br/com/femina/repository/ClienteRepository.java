d8debbbbeafe7198da5a628ba90fe1742b3ebc8a
VitorDanielAdams <vitor.adams@outlook.com>
2022-05-03 20:53:39 -0300

    feat - Criação da Classe Repositório de Cliente

---
+++ src/main/java/br/com/femina/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package br.com.femina.repository;
+
+import br.com.femina.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
