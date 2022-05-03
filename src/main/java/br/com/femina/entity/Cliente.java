57c7413e7f621d7ceeea3529238f96ec988a7b9d
VitorDanielAdams <vitor.adams@outlook.com>
2022-05-02 21:29:45 -0300

    feat - Criaç

---
+++ src/main/java/br/com/femina/entity/Cliente.java
@@ -0,0 +1,12 @@
+package br.com.femina.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cliente extends Usuario {
+}
