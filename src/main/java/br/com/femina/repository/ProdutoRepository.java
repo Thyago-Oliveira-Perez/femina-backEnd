dd17393287d927bf87e00b76c0da20dcc4b69106
VitorDanielAdams <vitor.adams@outlook.com>
2022-05-03 18:57:15 -0300

    feat - Criação da Classe Repositório do Produto

---
+++ src/main/java/br/com/femina/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package br.com.femina.repository;
+
+import br.com.femina.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
