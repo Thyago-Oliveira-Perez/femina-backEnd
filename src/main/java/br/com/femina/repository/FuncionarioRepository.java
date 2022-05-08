ebe1231a103a3c75b5d44211910a5c56ae7f4b54
VitorDanielAdams <vitor.adams@outlook.com>
2022-05-08 18:23:36 -0300

    feat - Criação da Classe Repositório de Funcionario

---
+++ src/main/java/br/com/femina/repository/FuncionarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.femina.repository;
+
+import br.com.femina.entity.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+}
