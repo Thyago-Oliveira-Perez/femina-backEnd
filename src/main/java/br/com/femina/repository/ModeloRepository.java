b5788e1f743fc3be757f8db6af4d84c5d8c01577
VitorDanielAdams <vitor.adams@outlook.com>
2022-05-08 18:06:22 -0300

    feat - Criação da Classe Repositório de Modelo

---
+++ src/main/java/br/com/femina/repository/ModeloRepository.java
@@ -0,0 +1,9 @@
+package br.com.femina.repository;
+
+import br.com.femina.entity.Modelo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModeloRepository extends JpaRepository<Modelo, Long> {
+}
