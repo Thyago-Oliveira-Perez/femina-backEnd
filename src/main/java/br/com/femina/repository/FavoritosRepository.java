4c95aa283ef6575538797bcc80355d7929b2adb3
VitorDanielAdams <vitor.adams@outlook.com>
2022-05-16 20:59:49 -0300

    feat - Criação da Classe Service de Favoritos sem validação

--- src/main/java/br/com/femina/repository/FavoritosRepository.java
+++ src/main/java/br/com/femina/repository/FavoritosRepository.java
@@ -1,9 +1,30 @@
 package br.com.femina.repository;
 
 import br.com.femina.entity.Favoritos;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FavoritosRepository extends JpaRepository<Favoritos, Long> {
+
+    @Query(value = "SELECT favoritos FROM Favoritos favoritos " +
+            "WHERE favoritos.habilitado = true",
+            countQuery = "SELECT count(1) FROM Favoritos",
+            nativeQuery = true
+    )
+    public Page<Favoritos> listAllActive(Pageable page);
+
+    @Modifying
+    @Query("UPDATE Favoritos favoritos " +
+            "SET favoritos.habilitado = :habilitado " +
+            "WHERE favoritos.id = :favoritos")
+    public void disable(
+            @Param("favoritos") Long idFavoritos,
+            @Param("habilitado") Boolean habilitado
+    );
 }
