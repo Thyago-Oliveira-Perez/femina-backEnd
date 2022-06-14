e4878045aff8ea47d2165cab1ebdb292f32efeb5
anna sganderla <sganderlakarlaanna@gmail.com>
2022-06-13 21:44:19 -0300

    add controller de cliente e funcionario

---
+++ src/main/java/br/com/femina/controller/ClienteController.java
@@ -0,0 +1,58 @@
+package br.com.femina.controller;
+
+import br.com.femina.entity.Cliente;
+import br.com.femina.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<Page<Cliente>> listAll(Pageable pageable) {
+        return ResponseEntity.ok().body(this.clienteService.findAll(pageable));
+    }
+
+    @GetMapping("/{idCliente}")
+    public ResponseEntity<Cliente> findById(@PathVariable("idCliente") Long idCliente){
+        return ResponseEntity.ok().body(this.clienteService.findById(idCliente).get());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> insertCliente(@RequestBody Cliente cliente) {
+        try{
+            this.clienteService.insert(cliente);
+            return ResponseEntity.ok().body("Cliente cadastrado com sucesso");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{idCliente}")
+    public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long idCliente ){
+        try{
+            this.clienteService.update(idCliente,cliente);
+            return ResponseEntity.ok().body("Funcionario atualizada com sucesso");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/status/{idCliente}")
+    public ResponseEntity<?> delete(@RequestBody Cliente cliente, @PathVariable Long idCliente ){
+        try{
+            this.clienteService.delete(idCliente, cliente);
+            return ResponseEntity.ok().body("Funcionario atualizada com sucesso");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}
--- src/main/java/br/com/femina/service/FuncionarioService.java
+++ src/main/java/br/com/femina/service/FuncionarioService.java
@@ -5,11 +5,13 @@ import br.com.femina.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Service
 public class FuncionarioService {
 
     @Autowired
