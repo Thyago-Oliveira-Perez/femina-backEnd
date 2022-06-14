559568f4d6e47593c6cbbd3e30ff308f3fed177c
Thyago_Oliveira_Perez <thyagodoliveiraperez@gmail.com>
2022-06-13 21:39:38 -0300

    feat - controller de categorias

---
+++ src/main/java/br/com/femina/controllers/CategoriaController.java
@@ -0,0 +1,48 @@
+package br.com.femina.controllers;
+
+import br.com.femina.entities.Categorias;
+import br.com.femina.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/api/categorias")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping("/{idCategoria}")
+    public ResponseEntity<Categorias> findById(@PathVariable("idCategoria") Long idCategoria){
+        return ResponseEntity.ok().body(this.categoriaService.findById(idCategoria).get());
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Categorias>> findAll(Pageable pageable){
+        return ResponseEntity.ok().body(this.categoriaService.listAll(pageable));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> insert(@RequestBody Categorias categorias){
+        try {
+            this.categoriaService.insert(categorias);
+            return ResponseEntity.ok().body("Categoria cadastrada com sucesso!");
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> delete(@PathVariable("idCategoria") Long idCategoria){
+        try {
+            this.categoriaService.delete(idCategoria);
+            return ResponseEntity.ok().body("Categoria deletada com sucesso!");
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
