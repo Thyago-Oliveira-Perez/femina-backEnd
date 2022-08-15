e7dc5ccb7ac754f35ca79487673cb29a3d143c86
Thyago_Oliveira_Perez <thyagodoliveiraperez@gmail.com>
2022-08-15 20:59:56 -0300

    fixed - listando todos os endpoints no swagger

--- src/main/java/br/com/femina/controllers/FuncionarioController.java
+++ src/main/java/br/com/femina/controllers/FuncionarioController.java
@@ -6,10 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("api/funcionarios")
 public class FuncionarioController {
