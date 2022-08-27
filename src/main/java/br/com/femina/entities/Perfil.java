96aad7fd911950c5a7fcaeead5c1c42cdca4d1db
Thyago_Oliveira_Perez <thyagodoliveiraperez@gmail.com>
2022-08-26 22:15:50 -0300

    faltou essas duas novas entidades

---
+++ src/main/java/br/com/femina/entities/Perfil.java
@@ -0,0 +1,25 @@
+package br.com.femina.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "perfil", schema = "public")
+public class Perfil implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter @Setter
+    @Column(name = "perfilName", nullable = false, length = 30, unique = true)
+    private String perfilName;
+
+    @Override
+    public String getAuthority() {
+        return this.perfilName;
+    }
+}
---
+++ src/main/java/br/com/femina/entities/Usuario.java
@@ -0,0 +1,91 @@
+package br.com.femina.entities;
+
+import br.com.femina.entities.enums.Sexo;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter @Setter
+    @Column(name = "name", nullable = false, length = 30, unique = true)
+    @NotNull(message = "Nome de usuário é obrigatório")
+    private String nome;
+
+    @Getter @Setter
+    @Column(name = "login", nullable = false, length = 30, unique = true)
+    @NotNull(message = "Login de usuário é obrigatório")
+    private String login;
+
+    @Getter @Setter
+    @Column(name = "password", nullable = false, length = 30, unique = true)
+    @NotNull(message = "Senha de usuário é obrigatório")
+    private String senha;
+
+    @Getter @Setter
+    @Column(name = "sexo", nullable = false, length = 15, unique = true)
+    @NotNull(message = "Sexo do usuário é obrigatório")
+    private Sexo sexo;
+
+    @Getter @Setter
+    @Column(name = "email", nullable = false, length = 30, unique = true)
+    @NotNull(message = "Email de usuário é obrigatório")
+    private String email;
+
+    @Getter @Setter
+    @Column(name = "telefone", nullable = false, length = 30, unique = true)
+    @NotNull(message = "Telefone do usuário é obrigatório")
+    @Pattern(regexp = "([0-9]{20})")
+    private String telefone;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Perfil> perfis = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
